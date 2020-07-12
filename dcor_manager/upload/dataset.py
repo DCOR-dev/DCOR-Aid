c03c8654ae10497d8f85b9e49ecdf23b4f6e58f6
Paul Müller <dev@craban.de>
2020-07-12 11:59:45 +0200

    implement removal of draft datasets

--- dcor_manager/upload/dataset.py
+++ dcor_manager/upload/dataset.py
@@ -13,8 +13,26 @@ def create_dataset(dataset_dict, server, api_key):
 
 
 def remove_draft(dataset_id, server, api_key):
+    """Remove a draft dataset"""
     api = CKANAPI(server=server, api_key=api_key)
     api.post("package_delete", {"id": dataset_id})
+
+
+def remove_all_drafts(server, api_key):
+    """Remove all draft datasets"""
+    api = CKANAPI(server=server, api_key=api_key)
+    user_dict = api.get_user_dict()
+    data = api.get(
+        "package_search",
+        q="*:*",
+        include_drafts=True,
+        include_private=True,
+        fq="creator_user_id:{} AND state:draft".format(user_dict["id"]),
+        rows=1000)
+    for dd in data["results"]:
+        assert dd["state"] == "draft"
+        remove_draft(dd["id"], server=server, api_key=api_key)
+    return data["results"]
 
 
 def activate_dataset(dataset_id, server, api_key):
