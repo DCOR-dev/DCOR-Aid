25617ebcd9dc32d46ea8a62c87ba842ad5fbf465
Paul Müller <dev@craban.de>
2021-04-08 01:17:05 +0200

    ci: control

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -23,6 +23,10 @@ def run_around_tests():
     time.sleep(2)
 
 
+def test_bad():
+    assert False
+
+
 def test_upload_simple(qtbot, monkeypatch):
     """Upload a test dataset"""
     mw = DCORAid()
