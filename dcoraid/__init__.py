b3cc479fcb9e34864a14c5ef38ea39bde371482e
Paul Müller <dev@craban.de>
2021-10-19 23:18:45 +0200

    fix: fatal typo in 0.7.4 release

--- dcoraid/__init__.py
+++ dcoraid/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa: F401
 from . import api, dbmodel, download, upload
 from .api import CKANAPI
-from dbmodel import APIInterrogator
+from .dbmodel import APIInterrogator
 from ._version import version as __version__
