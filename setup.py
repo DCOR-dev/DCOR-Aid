44a09d6b2fc961380a711e6c633e5738a1e274aa
Paul Müller <dev@craban.de>
2023-04-05 22:45:27 +0200

    setup: stop pinning dclab version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
     install_requires=[
-        "dclab[dcor]==0.48.4",  # pin for triage
+        "dclab[dcor]>=0.48.4",
         "numpy>=1.21",
         "requests>=2.13",
         "requests_cache",  # caching API-'get' requests
