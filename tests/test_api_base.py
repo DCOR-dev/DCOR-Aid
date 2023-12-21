d72f6f60e006535b35bb3d7b2df5a5799380d428
Paul Müller <dev@craban.de>
2023-12-21 11:53:08 +0100

    tests: fix perf_counter comparison

--- tests/test_api_base.py
+++ tests/test_api_base.py
@@ -6,7 +6,7 @@ from . import common
 def test_api_requests_cache_no_parameters():
     """Test the requests_cache for an API call *without* parameters"""
     api = common.get_api()
-    t0 = time.monotonic()
+    t0 = time.perf_counter()
     api.get("status_show")
     t1 = time.perf_counter()
     for ii in range(50):
@@ -18,7 +18,7 @@ def test_api_requests_cache_no_parameters():
 def test_api_requests_cache_with_parameters():
     """Test the requests_cache for an API call *with* parameters"""
     api = common.get_api()
-    t0 = time.monotonic()
+    t0 = time.perf_counter()
     api.get("organization_list_for_user", permission="create_dataset")
     t1 = time.perf_counter()
     for ii in range(50):
